3b63553aaaa9f67a65e6dd349733a72d4d198ba7
dokechin <user@user-PC.(none)>
2014-04-07 19:50:07 +0900

    Checking in changes prior to tagging of version 0.05.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 94bb46d..a8def9a 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Acme-Hakomonos-JP
    
     {{$NEXT}}
    
    +0.05 2014-04-07T10:49:45Z
    +
    +    - add SUNDOME,GRANDMESSE
    +
     0.04 2014-04-04T15:20:09Z
    
         - add OCC

--- lib/Acme/Hakomonos/JP.pm
+++ lib/Acme/Hakomonos/JP.pm
@@ -4,13 +4,15 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 use Acme::Hakomonos::Register (
     SORA_SCC    =>  {name => "札幌コンベンションセンター", cost => 206, size => 20300, url =>"http://www.sora-scc.jp/"},
     BUREAU      =>  {name => "静岡県東部地域コンベンションビューロー", cost => 114, size => 22742, url =>"http://www.shizutou-cb.jp/"},
     GRANSHIP    =>  {name => "グランシップ", cost => 706, size => 60360 , url => "http://www.granship.or.jp/"},
-    ROSETHEATER =>  {name => "富士市文化会館ロゼシアター ", cost => 162, size => 22794, url =>"http://rose-theatre.jp/"},
+    ROSETHEATER =>  {name => "富士市文化会館ロゼシアター", cost => 162, size => 22794, url =>"http://rose-theatre.jp/"},
+    SUNDOME     =>  {name => "サンドーム福井", cost => 221, size => 27917, url =>"http://www.sankan.jp/sundome/"},
+    GRANDMESSE  =>  {name => "グランメッセ熊本", cost => 183, size => 20280, url =>"http://www.grandmesse.jp/"},
     OCC         =>  {name => "沖縄コンベンションセンター", cost => 108, size =>20928, url =>"http://www.oki-conven.jp/"},
 );
 
